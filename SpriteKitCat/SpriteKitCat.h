f2b0954fd722e54974837d53dee27a7029657b1f
Johan Jordaan <djjordaan@gmail.com>
2014-06-16 11:13:21 +0200

    Initial Commit

---
+++ SpriteKitCat/SpriteKitCat.h
@@ -0,0 +1,19 @@
+//
+//  SpriteKitCat.h
+//  SpriteKitCat
+//
+//  Created by Johan Jordaan on 2014/06/16.
+//  Copyright (c) 2014 Johan Jordaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpriteKitCat.
+FOUNDATION_EXPORT double SpriteKitCatVersionNumber;
+
+//! Project version string for SpriteKitCat.
+FOUNDATION_EXPORT const unsigned char SpriteKitCatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpriteKitCat/PublicHeader.h>
+
+
